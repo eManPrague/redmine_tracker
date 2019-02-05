7ba09877eb81bf9617271b5b5cea70b3e4668ecf
Jiří Zdvomka <jiri.zdvomka@ackee.cz>
2019-02-05 15:09:08 +0100

    rename hours

--- app/utils/RedmineClient.js
+++ app/utils/RedmineClient.js
@@ -187,7 +187,7 @@ class RedmineClient {
     const timeEntry = {
       time_from_hours: moment.unix(entry.startTime).format('HH:mm'),
       time_to_hours: moment.unix(entry.endTime).format('HH:mm'),
-      original_hours: hours,
+      hours: hours,
       spent_on: moment.unix(entry.endTime).format('YYYY-MM-DD'),
       comments: entry.description,
       activity_id: entry.activity
